9067efc3863a26e8359c8d95146ccf94993ed87e
JamesBond2002 <nishkarsharora2002@gmail.com>
2021-03-06 13:56:34 +0530

    Update page1, made page2

--- JS/script.js
+++ JS/script.js
@@ -46,3 +46,44 @@ setTimeout(() => {
   }, 500);
 }, 1500);
 
+
+let leftSide = [
+  document.querySelector('#left-side'),
+  document.querySelector('.intro-text')
+];
+let rightSide = [
+  document.querySelector('#right-side'),
+  document.querySelector('.intro-img')
+];
+window.addEventListener('scroll', () => {
+  leftSide.forEach(x => {
+    x.style.left = -window.pageYOffset + 'px';
+  });
+  rightSide.forEach(x => {
+    x.style.right = -window.pageYOffset + 'px';
+  });
+  rightSide[0].style.left = window.pageYOffset + 'px';
+  rightSide[1].style.left = (window.innerWidth/2 + window.pageYOffset) + 'px';
+});
+
+let container = document.querySelectorAll('.container'), 
+box = document.querySelectorAll('.box');
+
+for(let i = 0; i<3; i++) {
+  container[i].addEventListener('mousemove', (e) => {
+    let rect = container[i].getBoundingClientRect(), 
+    x = e.clientX - rect.left, y = e.clientY - rect.top;
+    // console.log(rect.left, rect.right);
+    let xPos = ((rect.right-rect.left)/2 - x)/25;
+    let yPos = ((rect.bottom - rect.top)/2 - y)/25;
+    console.log(xPos, yPos);
+    box[i].style.transform = `rotateX(${yPos}deg) rotateY(${-xPos}deg)`;
+  });
+  container[i].addEventListener('mouseenter', () => {
+    box[i].style.transition = 'none';
+  });
+  container[i].addEventListener('mouseleave', () => {
+    box[i].style.transition = 'all .5s ease';
+    box[i].style.transform = `rotateX(0deg) rotateY(0deg)`;
+  });
+}
