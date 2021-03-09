c3c3fac540bb2ddcd6b2c6cb8346f61d93679f77
JamesBond2002 <nishkarsharora2002@gmail.com>
2021-03-09 13:08:37 +0530

    Wrote about projects

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,52 @@
+let leftSide = [
+  document.querySelector('#left-side'),
+  document.querySelector('.intro-text')
+];
+let rightSide = [
+  document.querySelector('#right-side'),
+  document.querySelector('.intro-img')
+];
+
+let amount = window.pageYOffset / window.innerHeight;
+amount *= window.innerWidth;
+leftSide.forEach(x => {
+  x.style.left = -amount + 'px';
+});
+rightSide[0].style.left = amount + 'px';
+rightSide[1].style.left = (window.innerWidth/2 + amount) + 'px';
+
+let scrollApear = (t) => {
+  let element = document.querySelector(t);
+  if(element.getBoundingClientRect().top 
+    < window.innerHeight/1.5)
+  {
+    element.style.opacity = 1;
+    element.style.transform = `translateX(0)`;
+  }
+};
+
+scrollApear('.project1 .box');
+scrollApear('.project2 .box');
+scrollApear('.project3 .box');
+scrollApear('#about-project1');
+scrollApear('#about-project2');
+scrollApear('#about-project3');
+
+window.addEventListener('scroll', () => {
+  let amount = window.pageYOffset / window.innerHeight;
+  amount *= window.innerWidth;
+  leftSide.forEach(x => {
+    x.style.left = -amount + 'px';
+  });
+  rightSide[0].style.left = amount + 'px';
+  rightSide[1].style.left = (window.innerWidth/2 + amount) + 'px';
+});
+
+window.addEventListener('load', () => {
+  let pl = document.querySelector('.preloader');
+  pl.classList.add('preload-finish');
+});
+
 let fadeOut = (to, setText) => {
   to.style.opacity = 0;
   to.style.transform = 'translateY(2rem)';
@@ -8,21 +57,11 @@ let fadeOut = (to, setText) => {
   }, 500);
 };
 
-let scrollApear = (t) => {
-  let element = document.querySelector(t);
-  console.log(element);
-  if(element.getBoundingClientRect().top 
-    < window.innerHeight/1.5)
-    element.classList.add('appear-horizontal');
-};
-
 
 let introText = document.querySelector('.intro-text').children[0];
 
 let hb = document.querySelector('.hamburger'); 
 let navLinks = document.querySelector('.nav-links');
-
-console.log(navLinks)
 
 introText.style.opacity = 1;
 introText.style.transform = 'translateY(0)';
@@ -31,7 +70,7 @@ setTimeout(() => {
   introText.style.transition = 'all 0.5s ease-out';
   introText.style.opacity = 0;
   introText.style.transform = 'translateY(2rem)';
-  console.log(1);
+  // console.log(1);
   setTimeout(() => {
         introText.innerHTML = `I am <span>Nishkarsh Arora</span>`;
         introText.style.transition = 'all 0.8s ease-out';
@@ -59,24 +98,6 @@ setTimeout(() => {
   }, 500);
 }, 1500);
 
-let leftSide = [
-  document.querySelector('#left-side'),
-  document.querySelector('.intro-text')
-];
-let rightSide = [
-  document.querySelector('#right-side'),
-  document.querySelector('.intro-img')
-];
-window.addEventListener('scroll', () => {
-  let amount = window.pageYOffset / window.innerHeight;
-  amount *= window.innerWidth;
-  leftSide.forEach(x => {
-    x.style.left = -amount + 'px';
-  });
-  rightSide[0].style.left = amount + 'px';
-  rightSide[1].style.left = (window.innerWidth/2 + amount) + 'px';
-});
-
 let container = document.querySelectorAll('.container'), 
 box = document.querySelectorAll('.box');
 
@@ -84,10 +105,9 @@ for(let i = 0; i<3; i++) {
   container[i].addEventListener('mousemove', (e) => {
     let rect = container[i].getBoundingClientRect(), 
     x = e.clientX - rect.left, y = e.clientY - rect.top;
-    // console.log(rect.left, rect.right);
     let xPos = ((rect.right-rect.left)/2 - x)/25;
     let yPos = ((rect.bottom - rect.top)/2 - y)/25;
-    console.log(xPos, yPos);
+    // console.log(xPos, yPos);
     box[i].style.transform = `rotateX(${yPos}deg) rotateY(${-xPos}deg)`;
   });
   container[i].addEventListener('mouseenter', () => {
@@ -100,9 +120,21 @@ for(let i = 0; i<3; i++) {
 };
 
 hb.addEventListener('click', () => {
-  console.log('hogya');
   navLinks.classList.toggle('open');
 });
+
+let navs = document.querySelectorAll('.navs');
+
+navs[0].addEventListener('click', () => {
+  window.scrollTo(0, 0);
+  navLinks.classList.toggle('open');
+})
+
+for(let i = 1; i<4; i++) {
+  navs[i].addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+  });
+}
 
 window.addEventListener('scroll', () => {
   scrollApear('.project1 .box');
